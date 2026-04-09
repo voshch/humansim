d360a3755561392571ebcf581898ad53b6c2b7a3
R4C00n <ductaingn.015203@gmail.com>
2026-04-09 17:50:15 +0800

    auto documenting custom behavior nodes

--- arena_humansim/arena_humansim/behavior/__init__.py
+++ arena_humansim/arena_humansim/behavior/__init__.py
@@ -1,3 +1,8 @@
-from .compiler import compile_agent_behavior
+def compile_agent_behavior(*args, **kwargs):
+    # Avoids circular import
+    from .compiler import compile_agent_behavior as _compile
+
+    return _compile(*args, **kwargs)
+
 
 __all__ = ["compile_agent_behavior"]
